7d7e32779ea665c2cc05a3805950cfca8651882f
marcos <marcoscorralescastro.daw2020@riberadeltajo.es>
2023-01-02 19:07:38 +0100

    Conecto cliente y servidor

--- server/server.js
+++ server/server.js
@@ -1,12 +1,12 @@
 import express from "express";
-import * as dotenv from 'dotenv';
-import cors from 'cors';
+import * as dotenv from "dotenv";
+import cors from "cors";
 import { Configuration, OpenAIApi } from "openai";
 
 dotenv.config();
 
-const configuration = new Configuration ({
-    apiKey: process.env.OPENAI_API_KEY,
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
 });
 
 const openai = new OpenAIApi(configuration);
@@ -15,32 +15,35 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/', async (req, res) => {
-    res.status(200).send({
-        message: 'Hello from CodeX',
-    })
+app.get("/", async (req, res) => {
+  res.status(200).send({
+    message: "Hello from CodeX!",
+  });
 });
 
-app.post('/', async(req, res) => {
-    try{
-        const promt = req.body.promt;
+app.post("/", async (req, res) => {
+  try {
+    const prompt = req.body.prompt;
 
-        const response = await openai.createCompletion({
-            model:"text-davinci-003",
-            prompt:`${promt}`,
-            temperature:0,
-            max_tokens:3000,
-            top_p:1,
-            frequency_penalty:0.5,
-            presence_penalty:0,
-        });
-        res.status(200).send({
-            bot: response.data.choices[0].text
-        })
-    }catch (error){
-        console.log(error);
-        res.status(500).send({ error })
-    }
-})
+    const response = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: `${prompt}`,
+      temperature: 0,
+      max_tokens: 3000,
+      top_p: 1,
+      frequency_penalty: 0.5,
+      presence_penalty: 0,
+    });
 
-app.listen(5000, () => console.log('Server is running on port http://localhost:5000/'));
+    res.status(200).send({
+      bot: response.data.choices[0].text,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send(error || "Something went wrong");
+  }
+});
+
+app.listen(5000, () =>
+  console.log("AI server started on http://localhost:5000")
+);
